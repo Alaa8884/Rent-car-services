d0ef1188ad23b994fa588d8fc1952a9317d8d9e0
Alaa Mohammed Youssef <alaamuhamed8@gamil.com>
2024-10-18 12:52:51 +0300

    Build find car form at hero section

---
+++ src/components/ui/FindCarForm.jsx
@@ -0,0 +1,51 @@
+import { Form, FormGroup } from "reactstrap";
+import "../../styles/findCarForm.css"
+function FindCarForm() {
+  return (
+    <Form className="form">
+      <div className=" d-flex align-items-center justify-content-between flex-wrap">
+        <FormGroup className="form-group">
+          <input
+            type="text"
+            name="from-address"
+            id="from-address"
+            placeholder="From Address"
+          />
+        </FormGroup>
+
+        <FormGroup className="form-group">
+          <input
+            type="text"
+            name="to-address"
+            id="to-address"
+            placeholder="to Address"
+          />
+        </FormGroup>
+        <FormGroup className="form-group">
+          <input type="date" name="journey-date" id="journey-date" />
+        </FormGroup>
+        <FormGroup className="form-group">
+          <input
+            type="time"
+            name="journey-time"
+            id="journey-time"
+            placeholder="Journey Time"
+          />
+        </FormGroup>
+        <FormGroup className="select-group">
+          <select name="car-type" id="car-type">
+            <option value="Ac car">Ac Car</option>
+            <option value="Non Ac car">Non Ac Car</option>
+          </select>
+        </FormGroup>
+        <FormGroup className="form-group">
+          <button className="find-btn" type="submit">
+            Find Car
+          </button>
+        </FormGroup>
+      </div>
+    </Form>
+  );
+}
+
+export default FindCarForm
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
 import HeroSlider from "../components/ui/HeroSlider";
 import Helmet from "../components/helmet/Helmet";
+import { Col, Container, Row } from "reactstrap";
+import FindCarForm from "../components/ui/FindCarForm";
 
 function Home() {
   return (
     <Helmet title="Home">
       <section className="p-0 hero-slider-section">
         <HeroSlider />
+        <div className="hero-form mt-5">
+          <Container className="form-container">
+            <Row>
+              <Col lg="4" md="4" sm="12" className=" px-3">
+                <div className="find-left">
+                  <h2>Find your best car here</h2>
+                </div>
+              </Col>
+              <Col lg="8" md="8" sm="12">
+                <FindCarForm />
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </section>
     </Helmet>
   );
