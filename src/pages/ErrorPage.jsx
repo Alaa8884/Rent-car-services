6b38d08b933bc8740eb8e9dc4b169340a933d4eb
Alaa Mohammed Youssef <alaamuhamed8@gamil.com>
2024-10-22 20:32:40 +0300

    Build simple Error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import "../styles/under-construction.css";
+
 function ErrorPage() {
+  const navigate = useNavigate();
   return (
-    <div>
-      Error page
-    </div>
-  )
+    <section className="under-construction-section d-flex align-items-center justify-content-center flex-column">
+      <h2 className=" mb-4">Sorry!! Some thing went wrong</h2>
+      <p>We apologize for any inconvenience. Please Try again later or click go back below.</p>
+      <button onClick={() => navigate(-1)}>Go Back</button>
+    </section>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
